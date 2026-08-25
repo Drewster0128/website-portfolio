19af7ae8e790d3f2c595534e62dcb2f30eac2d7e
Drewster0128 <acook21@hawk.iit.edu>
2026-08-25 14:54:08 -0500

    Begin work experience

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import Home from "./components/Home.vue"
+import WorkExperience from './components/WorkExperience.vue';
 </script>
 
 <template>
   <Home />
+  <WorkExperience />
 </template>
---
+++ src/components/WorkExperience.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="container container--column hero">
+        <h1 class="hero__title">Work Experience</h1>
+        <p class="hero__paragraph">[Template] A chronological ledger of professional developments, data architectures, blah blah blah</p>
+    </section>
+    <section class="container container--column">
+        <article class="experience">
+            <h2 class="experience__title">Data Science Intern</h2>
+            <h3 class="experience__employer">LabelMaster</h3>
+            <ul class="container container--column">
+                <li class="experience__bullet">
+                    Collaborated with a team of five to build an LSTM model using TensorFlow to forecast sales across multiple departments
+                </li>
+                <li class="experience__bullet">
+                    Connected seven trained LSTM models with frontend interface running with streamlit
+                </li>
+                <li class="experience__bullet">
+                    Scripted installation applications to ensure clean installs on all computers and laptops regardless of operating system
+                </li>
+                <li class="experience__bullet">
+                    Documented how-to guides describing step-by-step processes to output predictions and retrain models
+                </li>
+                <li class="experience__bullet">
+                    Visualized results using various graphs generated with Matplotlib
+                </li>
+                <li class="experience__bullet">
+                    Achieved average accuracy between all departments of 85-90%
+                </li>
+            </ul>
+        </article>
+        <article class="experience">
+            <h2 class="experience__title">IT Help Desk Intern</h2>
+            <h3 class="experience__employer">Clausen Miller P.C</h3>
+            <ul class="container container--column">
+                <li class="experience__bullet">
+                    Set up workstations for new employees with monitors, docking station, desk phones, ethernet, and access to office printers
+                </li>
+                <li class="experience__bullet">
+                    Assisted attorneys and secretaries over the phone for any tech related issues, including connecting to the company's VPN, installing drivers for personal printers, and teaching tips and tricks for Microsoft Office products like Word and Excel
+                </li>
+                <li class="experience__bullet">
+                    Wrote detailed descriptions on tickets defining the situation, who needed help, and how it was resolved
+                </li>
+                <li class="experience__bullet">
+                    Built a web user interface of printers and copiers separated by office location linking each printer to its status page for the IT staff to use on the company's intranet site
+                </li>
+                <li class="experience__bullet">
+                    Performed an audit on devices in the company's Active Directory, deleting any device that the company no longer uses, preventing any potential data breaches
+                </li>
+                <li class="experience__bullet">
+                    Consulted IT guides for common queries and updated them as needed
+                </li>
+            </ul>
+        </article>
+    </section>
+</template>
