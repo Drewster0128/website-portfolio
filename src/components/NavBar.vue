0d53a6d4097d76aad7dcc858707122e6353a838e
Drewster0128 <acook21@hawk.iit.edu>
2026-09-06 11:08:51 -0500

    Work on drop down

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,10 @@
 const props = defineProps(["active_page"]);
 import Resume from "../assets/AndewSCookResume-Generic-8-21-26.pdf";
 
-
+const resumeButtonHandler = () => {
+    const dropDown = document.getElementById("resume_drop_down");
+    dropDown.classList.toggle("drop_down--open");
+}
 </script>
 
 <template>
@@ -17,8 +20,19 @@ import Resume from "../assets/AndewSCookResume-Generic-8-21-26.pdf";
             <a class="nav__item" :class="{ 'nav__item--selected': props.active_page === 'projects' }" href="#projects">Projects</a>
             <a class="nav__item" href="">Contact</a>
         </nav>
-        <div class="column column--flex column--end">
-            <a class="nav__button" target="_blank" :href="Resume">Resume</a>
+        <div class="column column--flex column--end drop_down" id="resume_drop_down">
+            <button class="drop_down__button" @click="resumeButtonHandler">
+                Resume
+                <i class="bi bi-chevron-right" id="arrow"></i>
+            </button>
+            <ul class="drop_down__menu">
+                <li>
+                    <a class="drop_down__item" :href="Resume">Generic</a>
+                </li>
+                <li>
+                    <a class="drop_down__item" href="">Data Science</a>
+                </li>
+            </ul>
         </div>
     </header>
 </template>
