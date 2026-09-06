88e46ae465462fc9619355901549ed5cb45a357a
Drewster0128 <acook21@hawk.iit.edu>
2026-09-06 10:06:08 -0500

    Revert "Sloppy fix for navbar on small screens"
    This reverts commit 1214ca1f09aa0a993a3ae88e0e189368799156f4.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,13 +8,14 @@ import Resume from "../assets/AndewSCookResume-Generic-8-21-26.pdf";
 
 <template>
     <header class="header container container--row container--between container--align_baseline">
-        <div class="column column--fle invisble md_display--visible">
+        <div class="column column--flex">
             <h2 class="nav__title">Andrew Cook</h2>
         </div>
-        <nav class="nav column column--flex-2 container container--row container--center md_flex_grow--1">
+        <nav class="nav column column--flex container container--row container--center">
             <a class="nav__item" :class="{ 'nav__item--selected': props.active_page === 'home' }" href="#home">Home</a>
             <a class="nav__item" :class="{ 'nav__item--selected': props.active_page === 'work' }" href="#work">Work</a>
             <a class="nav__item" :class="{ 'nav__item--selected': props.active_page === 'projects' }" href="#projects">Projects</a>
+            <a class="nav__item" href="">Contact</a>
         </nav>
         <div class="column column--flex column--end">
             <a class="nav__button" target="_blank" :href="Resume">Resume</a>
